afb6f96aca23f7c1833dc60bdc6e50a65daf90f4
coderighter2 <24811616+coderighter2@users.noreply.github.com>
2022-03-09 13:25:14 +0200

    added env file

--- src/store/modules/contracts.js
+++ src/store/modules/contracts.js
@@ -22,13 +22,13 @@ const getters = {
 const actions = {
   async fetchContracts({ commit, rootState }) {
     let web3 = rootState.accounts.web3;
-    let contract = new web3.eth.Contract(ContractsAbi, '0x57a8d59eff06381a9b0d709271871187ee1b418c');
+    let contract = new web3.eth.Contract(ContractsAbi, process.env.VUE_APP_CONTRACT_ADDRESS);
     commit("setContract", contract);
   },
   async fetchNfts({ commit, rootState }) {
     try {
       let web3 = rootState.accounts.web3;
-      let contract = new web3.eth.Contract(ContractsAbi, '0x57a8d59eff06381a9b0d709271871187ee1b418c');
+      let contract = new web3.eth.Contract(ContractsAbi, process.env.VUE_APP_CONTRACT_ADDRESS);
       const tokenCount = await contract.methods.totalSupply().call()
       commit("setTokenCount", tokenCount);
       const nfts = []
